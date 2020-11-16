0e026651a98a18ffed41c42645aa5db19f431776
Haoyu Wang (FA Talent) <v-wanghaoyu@microsoft.com>
2020-11-16 12:06:06 +0800

    update

--- temp/test/build.py
+++ temp/test/build.py
@@ -33,13 +33,19 @@ def load_data(filename, step):
     val_split = int(round(0.9 * data.shape[1]))
     test_split = int(round(0.1 * data.shape[1])) 
     
-    gt_test = normalize(data[:,val_split:])
+    gt_test = data[:,val_split:]
     x_train = normalize(data[:,:train_split])
     y_train = normalize(data[:,day:train_split+day])
     x_val = normalize(data[:,:val_split])
     y_val = normalize(data[:,day:val_split+day])
     x_test = normalize(data[:, val_split-day:-day])
     y_test = normalize(data[:, val_split:])
+    # x_train = data[:,:train_split]
+    # y_train = data[:,day:train_split+day]
+    # x_val = data[:,:val_split]
+    # y_val = data[:,day:val_split+day]
+    # x_test = data[:,:-day]
+    # y_test = data[:,day:]
     
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
     x_val = np.reshape(x_val, (x_val.shape[0], x_val.shape[1], 1))
