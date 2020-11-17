8aabeb6722d990ceff7209e0dd0a6a98ed8c660e
Haoyu Wang (FA Talent) <v-wanghaoyu@microsoft.com>
2020-11-17 16:31:10 +0800

    update

--- build_crsp.py
+++ build_crsp.py
@@ -14,11 +14,11 @@ stock_list =  ['BHP', 'AMZN', 'CVX', 'BAC', 'JNJ', 'BA', 'KO', 'AAPL', 'CHL', 'D
               'SYT', 'HD', 'TOT', 'SPY', 'PFE', 'MMM', 'PG', 'MSFT', 'T', 'NGG',
               'TM', 'XOM', 'WFC', 'UNH', 'UPS', 'WMT', 'ORCL', 'VZ', 'SO'
            ]
-all_data = np.zeros((len(stock_list), 2515, 5), dtype = np.float32 )
+all_data = np.zeros((len(stock_list), 2515, 6), dtype = np.float32 )
 for i in range(len(stock_list)):
     stock = stock_list[i]
     print(stock)
-    stock_df = df[df['instrument'] == stock][['$open','$close','$high','$low','$volume']]
+    stock_df = df[df['instrument'] == stock][['$open','$close','$high','$low','$volume','$ratio']]
     prices = np.array(stock_df)
     all_data[i] = prices
 
