c43d87c7afcef7b777e0ae63ca9fb46f2c54be2e
Haoyu Wang (FA Talent) <v-wanghaoyu@microsoft.com>
2020-11-17 14:23:06 +0800

    shape

--- clear/test/test.py
+++ clear/test/test.py
@@ -84,6 +84,7 @@ if __name__=='__main__':
     #denormalization   
     prediction = (prediction[:,:,0] * (max_data[:,:,0] - min_data[:,:,0]) + (max_data[:,:,0] + min_data[:,:,0]))/2
     print prediction.shape
+    print gt_test.shape
     error = np.sum((prediction[:,:,0] - gt_test[:,:,0])**2) / (prediction.shape[1]* prediction.shape[0])
     print 'The mean square error is: %f' % error 
     if args.visualization:
