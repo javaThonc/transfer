f52473c85f561595230944ea298a8ba6a545e437
Haoyu Wang (FA Talent) <v-wanghaoyu@microsoft.com>
2020-11-24 14:49:34 +0800

    sdf

--- gru_for_v2_data.py
+++ gru_for_v2_data.py
@@ -152,7 +152,7 @@ class LSTM(nn.Module):
         return self.fc_out(out[:, -1, :]).squeeze()
 
 class SFM(nn.Module):
-    def __init__(self, d_feat=6, output_dim = 16, freq_dim = 5, hidden_size = 64, num_layers = 1,dropout_W = 0.0, dropout_U = 0.0):
+    def __init__(self, d_feat=6, output_dim = 16, freq_dim = 20, hidden_size = 64, num_layers = 1,dropout_W = 0.0, dropout_U = 0.0):
         super().__init__()
 
         self.input_dim  = d_feat
@@ -491,7 +491,7 @@ def main(args):
 
     suffix = "%s_dh%s_dn%s_drop%s_lr%s_bs%s_seed%s%s_label%s_dset%sfreq%soutput%s"%(
         args.model_name, args.hidden_size, args.num_layers, args.dropout,
-        args.lr, args.batch_size, args.seed, args.annot, args.label, args.dset, 5, 16
+        args.lr, args.batch_size, args.seed, args.annot, args.label, args.dset, 15, 8
     )
     if args.loss != 'logcosh':
         suffix += '_loss%s'%(args.loss)
@@ -589,11 +589,11 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # model
-    parser.add_argument('--model_name', default='LSTM')
+    parser.add_argument('--model_name', default='SFM')
     parser.add_argument('--d_feat', type=int, default=6)
     parser.add_argument('--hidden_size', type=int, default=64)
     parser.add_argument('--num_layers', type=int, default=2)
-    parser.add_argument('--dropout', type=float, default=0.1)
+    parser.add_argument('--dropout', type=float, default=0.5)
 
     # training
     parser.add_argument('--n_epochs', type=int, default=200)
